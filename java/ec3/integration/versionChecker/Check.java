bed6266afe5c3ec9d9c36d79c90175267529b66d
Modbder <Modbder@gmail.com>
2015-05-15 14:53:30 +0300

    Changing VersionChecker compathability by using the Curse project.

--- java/ec3/integration/versionChecker/Check.java
+++ java/ec3/integration/versionChecker/Check.java
@@ -1,12 +1,16 @@
 package ec3.integration.versionChecker;
 
+import net.minecraft.nbt.NBTTagCompound;
 import cpw.mods.fml.common.event.FMLInterModComms;
 
 public class Check {
 	
 	public static void checkerCommit()
 	{
-		FMLInterModComms.sendMessage("essentialcraft", "VersionChecker", "https://dl.dropbox.com/s/u49qiqb92rprq0v/version.txt?dl=1");
+		NBTTagCompound tag = new NBTTagCompound();
+		tag.setString("curseProjectName", "230148-essentialcraft-3");
+		tag.setString("curseFilenameParser", "EssentialCraftv[].jar");
+		FMLInterModComms.sendRuntimeMessage("essentialcraft", "VersionChecker","addCurseCheck", tag);
 	}
 
 }
