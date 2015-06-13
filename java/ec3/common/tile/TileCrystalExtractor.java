47c279576ef79c102d70255ab3d3bf530e415de2
Modbder <Modbder@gmail.com>
2015-06-13 18:42:27 +0300

    Crystal Extractor's 12th slot is now also accessable from the bottom, as other output slots.

--- java/ec3/common/tile/TileCrystalExtractor.java
+++ java/ec3/common/tile/TileCrystalExtractor.java
@@ -225,6 +225,6 @@ public class TileCrystalExtractor extends TileMRUGeneric{
 
 	@Override
 	public int[] getOutputSlots() {
-		return new int[]{1,2,3,4,5,6,7,8,9,10,11};
+		return new int[]{1,2,3,4,5,6,7,8,9,10,11,12};
 	}
 }
