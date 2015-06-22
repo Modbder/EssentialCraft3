0b761a313f6aed48b8142c3b92c2be386c355f30
Modbder <Modbder@gmail.com>
2015-06-22 11:29:31 +0300

    Removed the ability to duplicate bosses - can be turned back in the .cfg file Removed the duplication of inventory of clones - can be turned back in the .cfg file Fixed lag issue.

--- java/ec3/common/tile/TileMonsterHarvester.java
+++ java/ec3/common/tile/TileMonsterHarvester.java
@@ -3,6 +3,7 @@ package ec3.common.tile;
 import java.util.List;
 
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.boss.IBossDisplayData;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
@@ -23,11 +24,14 @@ import ec3.utils.common.ECUtils;
 public class TileMonsterHarvester extends TileMRUGeneric{
 	public static float rad = 12F;
 	public float rotation = 0F;
+	public int destrTick;
 	public static float cfgMaxMRU = ApiCore.DEVICE_MAX_MRU_GENERIC;
 	public static boolean generatesCorruption = false;
 	public static int genCorruption = 10;
 	public static int mruUsage = 100;
 	public static int mobDestructionTimer = 1440;
+	public static boolean allowBossDuplication = false;
+	public static boolean clearCopyInventory = true;
 	
 	public TileMonsterHarvester()
 	{
@@ -60,34 +64,50 @@ public class TileMonsterHarvester extends TileMRUGeneric{
 		}
 		if(!this.worldObj.isBlockIndirectlyGettingPowered(xCoord, yCoord, zCoord))
 		{
-	    	List<EntityLivingBase> lst = tile.getWorldObj().getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(tile.xCoord-16, tile.yCoord-16, tile.zCoord-16, tile.xCoord+16, tile.yCoord+16, tile.zCoord+16));
-	    	if(!lst.isEmpty() && !this.worldObj.isRemote)
-	    	{
-	    		for(int i = 0; i < lst.size(); ++i)
-	    		{
-	    			EntityLivingBase e = lst.get(i);
-	    			if(!(e instanceof EntityPlayer))
-	    			{
-	    				if(getMRU() > mruUsage && this.worldObj.getWorldTime() % mobDestructionTimer == 0)
-	    				{
-	    					EntityLivingBase copy = (EntityLivingBase) MiscUtils.cloneEntity(e);
-	    					this.worldObj.spawnEntityInWorld(copy);
-	    					FakePlayer player = new FakePlayer((WorldServer) e.worldObj, ECUtils.EC3FakePlayerProfile);
-	    					ItemStack stk = this.getStackInSlot(2);
-	    					if(stk != null)
-	    						player.inventory.setInventorySlotContents(player.inventory.currentItem, stk.copy());
-	    					copy.setHealth(0.1F);
-	    					player.attackTargetEntityWithCurrentItem(copy);
-	    					player.setDead();
-	    					this.setMRU(this.getMRU()-mruUsage);
-	    					if(copy.getHealth() > 0)
-	    						copy.setDead();
-	    	    			if(generatesCorruption)
-	    	    				ECUtils.increaseCorruptionAt(worldObj, xCoord, yCoord, zCoord, this.worldObj.rand.nextInt(genCorruption));
-	    				}
-	    			}
-	    		}
-	    	}
+			++destrTick;
+			if(destrTick >= mobDestructionTimer)
+			{
+				destrTick = 0;
+		    	List<EntityLivingBase> lst = tile.getWorldObj().getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(tile.xCoord-16, tile.yCoord-16, tile.zCoord-16, tile.xCoord+16, tile.yCoord+16, tile.zCoord+16));
+		    	if(!lst.isEmpty() && !this.worldObj.isRemote)
+		    	{
+		    		for(int i = 0; i < lst.size(); ++i)
+		    		{
+		    			EntityLivingBase e = lst.get(i);
+		    			if(!(e instanceof EntityPlayer))
+		    			{
+		    				if(getMRU() > mruUsage)
+		    				{
+		    					if(e instanceof IBossDisplayData && !allowBossDuplication)
+		    						return;
+		    					
+		    					EntityLivingBase copy = (EntityLivingBase) MiscUtils.cloneEntity(e);
+		    					this.worldObj.spawnEntityInWorld(copy);
+		    					if(clearCopyInventory)
+		    					{
+		    						copy.setCurrentItemOrArmor(0, null);
+		    						copy.setCurrentItemOrArmor(1, null);
+		    						copy.setCurrentItemOrArmor(2, null);
+		    						copy.setCurrentItemOrArmor(3, null);
+		    						copy.setCurrentItemOrArmor(4, null);
+		    					}
+		    					FakePlayer player = new FakePlayer((WorldServer) e.worldObj, ECUtils.EC3FakePlayerProfile);
+		    					ItemStack stk = this.getStackInSlot(2);
+		    					if(stk != null)
+		    						player.inventory.setInventorySlotContents(player.inventory.currentItem, stk.copy());
+		    					copy.setHealth(0.1F);
+		    					player.attackTargetEntityWithCurrentItem(copy);
+		    					player.setDead();
+		    					this.setMRU(this.getMRU()-mruUsage);
+		    					if(copy.getHealth() > 0)
+		    						copy.setDead();
+		    	    			if(generatesCorruption)
+		    	    				ECUtils.increaseCorruptionAt(worldObj, xCoord, yCoord, zCoord, this.worldObj.rand.nextInt(genCorruption));
+		    				}
+		    			}
+		    		}
+		    	}
+			}
 		}
 	}
     
@@ -102,7 +122,9 @@ public class TileMonsterHarvester extends TileMRUGeneric{
 	    			"Can this device actually generate corruption:false",
 	    			"The amount of corruption generated each tick(do not set to 0!):10",
 	    			"Radius to duplicate mobs within:12.0",
-	    			"Ticks required to duplicate mobs:1440"
+	    			"Ticks required to duplicate mobs:1440",
+	    			"Allow duplication of bosses:false",
+	    			"Remove inventory of a duplicate before killing it:true"
 	    			},"");
 	    	String dataString="";
 	    	
@@ -117,6 +139,8 @@ public class TileMonsterHarvester extends TileMRUGeneric{
 	    	genCorruption = Integer.parseInt(data[3].fieldValue);
 	    	rad = Float.parseFloat(data[4].fieldValue);
 	    	mobDestructionTimer = Integer.parseInt(data[5].fieldValue);
+	    	allowBossDuplication = Boolean.parseBoolean(data[6].fieldValue);
+	    	clearCopyInventory = Boolean.parseBoolean(data[7].fieldValue);
 	    	
 	    	cfg.save();
     	}catch(Exception e)
