01c6494fcdafcf72d415af10227921123f7f7812
Modbder <Modbder@gmail.com>
2015-07-09 16:09:26 +0300

    Api: Removing deprecated functions and adding some documentation

--- java/ec3/api/ApiCore.java
+++ java/ec3/api/ApiCore.java
@@ -14,7 +14,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.FakePlayer;
 
 /**
  * 
@@ -23,18 +22,41 @@ import net.minecraftforge.common.util.FakePlayer;
  */
 public class ApiCore {
 	
+	/**
+	 * A usual amount of MRU all generators would have
+	 */
 	public static final float GENERATOR_MAX_MRU_GENERIC = 10000F;
 	
+	/**
+	 * A usual amount of MRU all devices would have
+	 */
 	public static final float DEVICE_MAX_MRU_GENERIC = 5000F;
 	
+	/**
+	 * A list of all items, which allow the player to see MRUCUs and MRU particles
+	 */
 	public static List<Item> allowsSeeingMRU = new ArrayList<Item>();
 	
+	/**
+	 * A list of reductions the armor can have
+	 */
 	public static Hashtable<Item, ArrayList<Float>> reductionsTable = new Hashtable<Item, ArrayList<Float>>();
 	
+	/**
+	 * All categories the Book Of Knowledge can have
+	 */
 	public static List<CategoryEntry> categories = new ArrayList<CategoryEntry>();
 	
+	/**
+	 * A list of all discoveries bound to generic ItemStack
+	 */
 	public static Hashtable<String, DiscoveryEntry> discoveriesByIS = new Hashtable<String, DiscoveryEntry>();
 	
+	/**
+	 * Use this to get a full information on the player - it's UBMRU, Balance and Corruption status
+	 * @param p - the player to get the data of. Please check, that it is not null and is not a FakePlayer!
+	 * @return The corresponding player data, or null if something went wrong
+	 */
 	public static IPlayerData getPlayerData(EntityPlayer p)
 	{
 		try
@@ -49,21 +71,11 @@ public class ApiCore {
 		}
 	}
 	
-	@Deprecated
-	public static int getubmru(EntityPlayer p)
-	{
-		return getPlayerData(p).getPlayerUBMRU();
-	}
-	
-	@Deprecated
-	public static void setubmru(EntityPlayer p, int amount)
-	{
-		if(!(p instanceof FakePlayer))
-			getPlayerData(p).modifyUBMRU(amount);
-	}
-	
-	
-	
+	/**
+	 * Allows a specified block to be a part of a specified structure.
+	 * @param structure - the structure the block can be a part of
+	 * @param registered - the block that is registered. Not metadata sensitive!
+	 */
 	@SuppressWarnings("unchecked")
 	public static void registerBlockInAStructure(EnumStructureType structure, Block registered)
 	{
@@ -83,6 +95,12 @@ public class ApiCore {
 		}
 	}
 	
+	/**
+	 * Allows a block to 'resist' MRUCU effects on the player. Also that block will be tougher for the corruption no grow on
+	 * @param registered - the block to register
+	 * @param metadata - the block's metadata. Use -1 or OreDictionary.WILDCARD_VALUE to make the check ignore metadata.
+	 * @param resistance - the resistance the block will have. All non-registered have 1.
+	 */
 	public static void registerBlockMRUResistance(Block registered, int metadata, float resistance)
 	{
 		try
@@ -97,6 +115,11 @@ public class ApiCore {
 		}
 	}
 	
+	/**
+	 * Finds a DiscoveryEntry by the given ItemStack. The ItemStack would either be in the list of items at one of the pages, or will be a crafting result.
+	 * @param referal - the ItemStack to lookup.
+	 * @return A valid DiscoveryEntry if was found, null otherwise
+	 */
 	public static DiscoveryEntry findDiscoveryByIS(ItemStack referal)
 	{
 		if(referal == null)return null;
@@ -107,6 +130,10 @@ public class ApiCore {
 		return de;
 	}
 	
+	/**
+	 * Registers an item as one allowed to grant the player MRUCU and MRU vision
+	 * @param i - the item to register
+	 */
 	public static void allowItemToSeeMRU(Item i)
 	{
 		allowsSeeingMRU.add(i);
