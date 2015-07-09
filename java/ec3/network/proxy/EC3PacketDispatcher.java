22c529baee95d36a2b500e173e1155909e3147ff
Modbder <Modbder@gmail.com>
2015-07-09 16:15:39 +0300

    Added: New Magical Inventory Manager - much better than the previous, also with some nifty functions, like autocraftng!

--- java/ec3/network/proxy/EC3PacketDispatcher.java
+++ java/ec3/network/proxy/EC3PacketDispatcher.java
@@ -3,12 +3,19 @@ package ec3.network.proxy;
 import io.netty.channel.ChannelHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntity;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import ec3.client.gui.GuiNewMIMScreen;
 import ec3.common.mod.EssentialCraftCore;
+import ec3.common.tile.TileMagicalQuarry;
+import ec3.common.tile.TileNewMIM;
+import ec3.common.tile.TileNewMIMInventoryStorage;
+import ec3.common.tile.TileNewMIMScreen;
 import ec3.network.PacketNBT;
 import ec3.utils.common.ECUtils;
 
@@ -46,6 +53,97 @@ public class EC3PacketDispatcher implements IMessageHandler<PacketNBT,IMessage>{
 				ECUtils.ec3WorldTag = message.theTag;
 				break;
 			}
+			case 3:
+			{
+				TileEntity tile = EssentialCraftCore.proxy.getClientPlayer().worldObj.getTileEntity(message.theTag.getInteger("x"), message.theTag.getInteger("y"), message.theTag.getInteger("z"));
+				if(tile != null && tile instanceof TileNewMIMInventoryStorage)
+				{
+					TileNewMIMInventoryStorage.class.cast(tile).items.clear();
+					for(int i = 0; i < message.theTag.getTagList("items", 10).tagCount(); ++i)
+					{
+						ItemStack s = ItemStack.loadItemStackFromNBT(message.theTag.getTagList("items", 10).getCompoundTagAt(i));
+						s.stackSize = message.theTag.getTagList("items", 10).getCompoundTagAt(i).getInteger("stackSize");
+						TileNewMIMInventoryStorage.class.cast(tile).items.add(s);
+					}
+				}
+				break;
+			}
+			case 4:
+			{
+				TileEntity tile = EssentialCraftCore.proxy.getClientPlayer().worldObj.getTileEntity(message.theTag.getInteger("x"), message.theTag.getInteger("y"), message.theTag.getInteger("z"));
+				if(tile != null && tile instanceof TileMagicalQuarry)
+				{
+					TileMagicalQuarry.class.cast(tile).miningX = message.theTag.getInteger("mx");
+					TileMagicalQuarry.class.cast(tile).miningY = message.theTag.getInteger("my");
+					TileMagicalQuarry.class.cast(tile).miningZ = message.theTag.getInteger("mz");
+				}
+				break;
+			}case 5:
+			{
+				ItemStack retrieved = ItemStack.loadItemStackFromNBT(message.theTag.getCompoundTag("requestedItem"));
+				
+				EntityPlayerMP requester = MinecraftServer.getServer().getConfigurationManager().func_152612_a(message.theTag.getString("requester"));
+				
+				if(!message.theTag.getBoolean("craft"))
+					retrieved.stackSize = message.theTag.getInteger("size");
+				
+				int size = message.theTag.getInteger("size");
+				TileEntity tile = requester.worldObj.getTileEntity(message.theTag.getInteger("px"), message.theTag.getInteger("py"), message.theTag.getInteger("pz"));
+				if(tile != null && tile instanceof TileNewMIM)
+				{
+					TileNewMIM mim = TileNewMIM.class.cast(tile);
+					
+					if(!message.theTag.getBoolean("craft"))
+					{
+						int left = mim.retrieveItemStackFromSystem(retrieved,false,true);
+						if(left == 0)
+						{
+							
+						}else
+						{
+							size -= left;
+						}
+						TileEntity t = requester.worldObj.getTileEntity(message.theTag.getInteger("x"), message.theTag.getInteger("y"), message.theTag.getInteger("z"));
+						if(t != null && t instanceof TileNewMIMScreen)
+						{
+							TileNewMIMScreen sc = TileNewMIMScreen.class.cast(t);
+							int left64 = size % 64;
+							int times64 = size/64;
+							for(int i = 0; i < size; ++i)
+							{
+								sc.setMRU(sc.getMRU()-TileNewMIMScreen.mruForOut);
+							}
+							for(int i = 0; i < times64; ++i)
+							{
+								ItemStack added = retrieved.copy();
+								added.stackSize = 64;
+								if(!requester.inventory.addItemStackToInventory(added))
+									requester.dropPlayerItemWithRandomChoice(added, false);
+							}
+							ItemStack added = retrieved.copy();
+							added.stackSize = left64;
+							if(!requester.inventory.addItemStackToInventory(added))
+								requester.dropPlayerItemWithRandomChoice(added, false);
+							
+							NBTTagCompound canDo = new NBTTagCompound();
+							PacketNBT packet = new PacketNBT(canDo).setID(6);
+							EssentialCraftCore.network.sendTo(packet, requester);
+						}
+					}else
+					{
+						mim.craftFromTheSystem(retrieved, size);
+						NBTTagCompound canDo = new NBTTagCompound();
+						PacketNBT packet = new PacketNBT(canDo).setID(6);
+						EssentialCraftCore.network.sendTo(packet, requester);
+					}
+				}
+				break;
+			}case 6:
+			{
+				GuiNewMIMScreen.packetArrived = true;
+				break;
+			}
+			
 		}
 		return null;
 	}
