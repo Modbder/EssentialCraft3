af30f9781832c8b9129a8c0a5738b1af6fa2cddc
Modbder <Modbder@gmail.com>
2015-07-09 16:10:48 +0300

    Added a Config option to spawn the Hologram in the overworld

--- java/ec3/common/block/BlockHologramSpawner.java
+++ java/ec3/common/block/BlockHologramSpawner.java
@@ -17,7 +17,7 @@ public class BlockHologramSpawner extends Block{
 	
     public boolean onBlockActivated(World w, int x, int y, int z, EntityPlayer p, int p_149727_6_, float p_149727_7_, float p_149727_8_, float p_149727_9_)
     {
-    	if(!w.isRemote && w.provider != null && w.provider.dimensionId == Config.dimensionID)
+    	if(!w.isRemote && w.provider != null && (w.provider.dimensionId == Config.dimensionID || Config.allowHologramInOtherDimensions))
     	{
     		w.setBlockToAir(x, y, z);
     		EntityHologram hg = new EntityHologram(w);
