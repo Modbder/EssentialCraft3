ffcaec484f8dc1699e675f721a3027b084a6bf00
Modbder <Modbder@gmail.com>
2015-07-10 14:57:45 +0300

    Changed the way drops generate to use vanilla's methods instead of mod

--- java/ec3/common/world/WorldGenElementalDrops.java
+++ java/ec3/common/world/WorldGenElementalDrops.java
@@ -8,126 +8,114 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraft.world.gen.feature.WorldGenerator;
-import net.minecraftforge.common.util.ForgeDirection;
 
 public class WorldGenElementalDrops extends WorldGenerator{
 
-	public Block minableReplaceable;
-	public int dimID;
-	public int generationStep;
-	WorldGenElementalDrops parent;
+	public Block minableReplacable;
+	public boolean isEnd;
+	public boolean isNether;
+	public int generationSteps;
+	public Random rnd;
+	public int x,y,z;
+	public World w;
+	
+	public WorldGenElementalDrops selectValidAABBGen()
+	{
+		int startX = x;
+		int startZ = z;
+		if(!w.blockExists(x+generationSteps/2, y, z))
+		{
+			while(!w.blockExists(x+generationSteps/2-1, y, z) && x+generationSteps/2-1 >= startX)
+			{
+				--x;
+			}
+		}
+		if(!w.blockExists(x-generationSteps/2, y, z))
+		{
+			while(!w.blockExists(x-generationSteps/2+1, y, z) && x-generationSteps/2+1 <= startX)
+			{
+				++x;
+			}
+		}
+		if(!w.blockExists(x, y, z+generationSteps/2))
+		{
+			while(!w.blockExists(x, y, z+generationSteps/2-1) && z+generationSteps/2-1 >= startZ)
+			{
+				--z;
+			}
+		}
+		if(!w.blockExists(x, y, z-generationSteps/2))
+		{
+			while(!w.blockExists(x, y, z-generationSteps/2+1) && z-generationSteps/2+1 <= startZ)
+			{
+				++z;
+			}
+		}
+		return this;
+	}
+	
+	public WorldGenElementalDrops(World w, int type, int x, int y, int z)
+	{
+		rnd = w.rand;
+		if(type == 0)
+		{
+			isEnd = false;
+			isNether = false;
+			minableReplacable = Blocks.stone;
+		}
+		if(type == 1)
+		{
+			isEnd = true;
+			isNether = false;
+			minableReplacable = Blocks.end_stone;
+		}
+		if(type == -1)
+		{
+			isEnd = false;
+			isNether = true;
+			minableReplacable = Blocks.netherrack;
+		}
+		generationSteps = 16;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.w = w;
+	}
+	
+	@Override
+	public boolean generate(World w, Random rnd,int x, int y, int z)
+	{
+		
+
+		int meta = 0;
+		
+		if(isNether)
+			meta += 5;
+		if(isEnd)
+			meta += 10;
+		
+		new WorldGenMinable(BlocksCore.oreDrops,meta,16,minableReplacable).generate(w, rnd, x, y, z);
+		
+		new WorldGenMinable(BlocksCore.oreDrops,meta+1,8,minableReplacable).generate(w, rnd, x+rnd.nextInt(6)-rnd.nextInt(6), y+rnd.nextInt(6)-rnd.nextInt(6), z+rnd.nextInt(6)-rnd.nextInt(6));
+		new WorldGenMinable(BlocksCore.oreDrops,meta+2,8,minableReplacable).generate(w, rnd, x+rnd.nextInt(6)-rnd.nextInt(6), y+rnd.nextInt(6)-rnd.nextInt(6), z+rnd.nextInt(6)-rnd.nextInt(6));
+		new WorldGenMinable(BlocksCore.oreDrops,meta+3,8,minableReplacable).generate(w, rnd, x+rnd.nextInt(6)-rnd.nextInt(6), y+rnd.nextInt(6)-rnd.nextInt(6), z+rnd.nextInt(6)-rnd.nextInt(6));
+		new WorldGenMinable(BlocksCore.oreDrops,meta+4,8,minableReplacable).generate(w, rnd, x+rnd.nextInt(6)-rnd.nextInt(6), y+rnd.nextInt(6)-rnd.nextInt(6), z+rnd.nextInt(6)-rnd.nextInt(6));
+		
+		return false;
+	}
 	
 	public static void handleGeneration(Random random, int chunkX, int chunkZ, World world,IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
 	{
 		for(int i = 0; i < Config.oreGenAttempts; ++i)
 		{
-			int rndX = chunkX*16 + random.nextInt(16);
-			int rndY = random.nextInt(256);
-			int rndZ = chunkZ*16 + random.nextInt(16);
-			Block minableReplaceable = world.provider.dimensionId == -1 ? Blocks.netherrack : world.provider.dimensionId == 1 ? Blocks.end_stone : Blocks.stone;
-			WorldGenElementalDrops.staticGenerate(world, random, rndX, rndY, rndZ, world.provider.dimensionId, 0, minableReplaceable);
+			int x = chunkX*16 + random.nextInt(16);
+			int y = random.nextInt(world.getHeight());
+			int z = chunkZ*16 + random.nextInt(16);
+			int type = world.provider.isSurfaceWorld() ? 0 : world.provider.isHellWorld ? -1 : 1;
+			new WorldGenElementalDrops(world,type,x,y,z).selectValidAABBGen().generate(world, random, x, y, z);
 		}
-	}
-	
-	public WorldGenElementalDrops(int dim)
-	{
-		dimID = dim;
-		generationStep = 0;
-		minableReplaceable = dim == -1 ? Blocks.netherrack : dim == 1 ? Blocks.end_stone : Blocks.stone;
-		parent = this;
-	}
-	
-	protected WorldGenElementalDrops(int i, WorldGenElementalDrops j, Block k)
-	{
-		dimID = i;
-		parent = j;
-		minableReplaceable = k;
-	}
-	
-	@Override
-	public boolean generate(World w, Random r,int x, int y, int z) 
-	{
-		int metadata = 0;
-		if(parent.generationStep < 16)
-		{
-			metadata = dimID == 1 ? 10 : dimID == -1 ? 5 : 0;
-		}else
-		{
-			metadata = dimID == 1 ? 11+r.nextInt(4) : dimID == -1 ? 6+r.nextInt(4) : 1+r.nextInt(4);
-		}
-		if(!w.blockExists(x, y, z))
-			return false;
-		 
-		Block b = w.getBlock(x, y, z);
-		if(b.isReplaceableOreGen(w, x, y, z, minableReplaceable))
-		{
-			if(parent.generationStep < 16)
-			{
-				w.setBlock(x, y, z, BlocksCore.oreDrops,metadata,0);
-			}else
-			{
-				if(r.nextInt(parent.generationStep) <= 16)
-					w.setBlock(x, y, z, BlocksCore.oreDrops,metadata,0);
-				else
-				{
-					 w.setBlock(x, y, z, minableReplaceable,metadata,0);
-					return false;
-				}
-			}
-			++parent.generationStep;
-		}else
-			return false;
-		int tries = 0;
-		int i = r.nextInt(6);
-		ForgeDirection dir = ForgeDirection.VALID_DIRECTIONS[i];
-		while(!new WorldGenElementalDrops(dimID,parent,minableReplaceable).generate(w, r, x+dir.offsetX, y+dir.offsetY, z+dir.offsetZ) && tries < 8)
-		{
-			i = r.nextInt(6);
-			dir = ForgeDirection.VALID_DIRECTIONS[i];
-			++tries;
-		}
-		return true;
-	}
-	
-	public static int staticGenerate(World w, Random r,int x, int y, int z, int dimID, int generationStep, Block minableReplaceable) 
-	{
-		if(generationStep > 24)
-			return 24;
-		
-		int metadata = 0;
-		if(generationStep < 16)
-		{
-			metadata = dimID == 1 ? 10 : dimID == -1 ? 5 : 0;
-		}else
-		{
-			metadata = dimID == 1 ? 11+r.nextInt(4) : dimID == -1 ? 6+r.nextInt(4) : 1+r.nextInt(4);
-		}
-		if(!w.blockExists(x, y, z))
-			return generationStep;
-		 
-		Block b = w.getBlock(x, y, z);
-		if(b.isReplaceableOreGen(w, x, y, z, minableReplaceable))
-		{
-			if(generationStep < 16)
-			{
-				w.setBlock(x, y, z, BlocksCore.oreDrops,metadata,0);
-			}else
-			{
-				if(r.nextInt(generationStep) <= 16)
-					w.setBlock(x, y, z, BlocksCore.oreDrops,metadata,0);
-				else
-				{
-					 w.setBlock(x, y, z, minableReplaceable,metadata,0);
-					return ++generationStep;
-				}
-			}
-		}else
-			return generationStep;
-		int i = r.nextInt(6);
-		ForgeDirection dir = ForgeDirection.VALID_DIRECTIONS[i];
-		WorldGenElementalDrops.staticGenerate(w, r, x+dir.offsetX, y+dir.offsetY, z+dir.offsetZ, dimID, generationStep+1, minableReplaceable);
-		return generationStep;
 	}
 
 }
