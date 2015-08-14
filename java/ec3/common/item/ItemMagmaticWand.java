789de5b15b1733529c0c210d5c91203d2a52b374
Modbder <Modbder@gmail.com>
2015-08-14 18:31:27 +0300

    Changed: The Magmatic Wand now fires a BreakEvent, so it can be controlled via WorldGuard and similar plugins

--- java/ec3/common/item/ItemMagmaticWand.java
+++ java/ec3/common/item/ItemMagmaticWand.java
@@ -5,8 +5,12 @@ import ec3.utils.common.EnumOreColoring;
 import DummyCore.Utils.MathUtils;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings.GameType;
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class ItemMagmaticWand extends ItemStoresMRUInNBT {
@@ -39,18 +43,30 @@ public class ItemMagmaticWand extends ItemStoresMRUInNBT {
 					break;
 				}
 			}
-	        if(metadata != -1 && (ECUtils.tryToDecreaseMRUInStorage(player, -500) || this.setMRU(stack, -500)))
+	        if(!player.worldObj.isRemote && metadata != -1 && (ECUtils.tryToDecreaseMRUInStorage(player, -500) || this.setMRU(stack, -500)))
 	        {
 				int suggestedStackSize = EnumOreColoring.values()[metadata].dropAmount;
 				if(world.rand.nextFloat() <= 0.33F)
 					suggestedStackSize = EnumOreColoring.values()[metadata].dropAmount*2;
 				ItemStack sugStk = new ItemStack(ItemsCore.magicalAlloy,suggestedStackSize,metadata);
-				world.setBlockToAir(px, y, pz);
-				EntityItem drop = new EntityItem(world, px, y, pz, sugStk);
-				drop.motionX = MathUtils.randomDouble(world.rand)/10;
-				drop.motionY = MathUtils.randomDouble(world.rand)/10;
-				drop.motionZ = MathUtils.randomDouble(world.rand)/10;
-				world.spawnEntityInWorld(drop);
+				
+				GameType type = GameType.SURVIVAL;
+				if(player.capabilities.isCreativeMode)
+					type = GameType.CREATIVE;
+				if(!player.capabilities.allowEdit)
+					type = GameType.ADVENTURE;
+				
+				BreakEvent be = ForgeHooks.onBlockBreakEvent(player.worldObj, type, (EntityPlayerMP) player, px, y, pz);
+				if(!be.isCanceled())
+				{
+					world.setBlockToAir(px, y, pz);
+					EntityItem drop = new EntityItem(world, px, y, pz, sugStk);
+					drop.motionX = MathUtils.randomDouble(world.rand)/10;
+					drop.motionY = MathUtils.randomDouble(world.rand)/10;
+					drop.motionZ = MathUtils.randomDouble(world.rand)/10;
+					world.spawnEntityInWorld(drop);
+				}
+				
 				player.swingItem();
 	        }
 		}
